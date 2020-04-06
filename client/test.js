9053ce16c3e94e18e426f8270e0f59c6c3785c78
kyven <kyven@seas.upenn.edu>
2020-04-06 01:48:27 -0400

    clean branch

--- client/test.js
+++ client/test.js
@@ -1,6 +1,14 @@
-var password = require('./tools/password.js');
+var fs = require('fs');
+var sharp = require('sharp');
+
+var img = fs.readFileSync('./test.jpg');
+console.log(img);
+
+var resizer = sharp()
+    .resize({
+        width: 250,
+        height: 250
+    });
 
 
-console.log(password.hashAndSalt('test'));
-
-console.log(password.authenticate('test', 'b1d022507eba90787641f197c3e273177fa4ee85b90f25ba57236c96bc29ccaf', 'd00c9071e801dff011b6e2c23111ccccc0dbc22ccaf33ad45a02d555e9b14f8c'))
+//console.log(out);
