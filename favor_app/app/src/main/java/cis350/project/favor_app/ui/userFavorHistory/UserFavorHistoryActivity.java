af32846d1755f34bafe8439b5afb707e1f507d17
kyven <kyven@seas.upenn.edu>
2020-04-14 00:21:53 -0400

    start implementing favor history

---
+++ favor_app/app/src/main/java/cis350/project/favor_app/ui/userFavorHistory/UserFavorHistoryActivity.java
@@ -0,0 +1,14 @@
+package cis350.project.favor_app.ui.userFavorHistory;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import cis350.project.favor_app.R;
+
+public class UserFavorHistoryActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_favors);
+    }
+}
